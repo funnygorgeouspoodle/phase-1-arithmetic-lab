a3e61394ee5963267c96f82bacdf4a912b817a27
Ashiq Legi <ashiqlegi@gmail.com>
2021-12-04 21:22:53 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let multiply
+let num1=62
+let num2=1
+multiply = num1 * num2;
 
-// Write your code here
+let random 
+random = Math.floor(Math.random()*10);
+
+let mod
+let num3 = 11
+let num4= 7
+mod = num3%num4;
+
+let max 
+max = Math.max(1,2,4,20);
+
